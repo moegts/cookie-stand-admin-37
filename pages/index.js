2bf16073c2d9ee144b58e578a0217c35962cee9f
Mohammad Khalil <moegts@gmail.com>
2021-12-23 12:50:22 +0200

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,81 @@
 import Head from 'next/head'
+import { useState } from 'react';
 
 export default function Home() {
+
+  const [data, dataList] = useState([])
+  const [location, locationList] = useState(['x']);
+  const [minCustomers, minCustomersList] = useState(['x']);
+  const [maxCustomers, maxCustomersList] = useState(['x']);
+  const [avgCoockies, avgCoockiesList] = useState(['x']);
+
+  const formHandler = (e) => {
+    e.preventDefault();
+    let newData = {
+      location: e.target.location.value,
+      minCustomers: e.target.minimum.value,
+      maxCustomers: e.target.maximum.value,
+      avgCoockies: e.target.average.value
+    };
+    dataList([...data, newData])
+    locationList(newData.location)
+    minCustomersList(newData.minCustomers)
+    maxCustomersList(newData.maxCustomers)
+    avgCoockiesList(newData.avgCoockies)
+  }
+  
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
+    <>
+      <head>
+        <title>cookie stand admin</title>
         <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
-    </div>
+      </head>
+      <body>
+        <header>
+          <div className='flex justify-between bg-teal-600 py-6 items-center'>
+            <h1 className='text-3xl font-bold mx-1'>Cookie Stand Admin</h1>
+          </div>
+        </header>
+        <main>
+          <div className='w-9/12 bg-teal-400 mx-auto my-5 p-2 rounded'>
+            <div className='p-2'>
+              <h2 className='flex justify-center text-1xl font-bold mx-1 my-3'>Create Cookie Stand</h2>
+              <form onSubmit={formHandler}>
+                <span className='flex justify-center'>
+                  <label className='mx-6' htmlFor="location">Location:</label>
+                  <input name='location' id='location' type="text" className=' justify-center w-4/5 mx-auto my-1/4 h-4' />
+                </span>
+                <div className='text-[0.8rem] flex justify-center flex-row space-x-4 space-x-reverse grid gap-4 grid-cols-4'>
+                  <span >
+                    <label className='text-[0.8rem] flex justify-center text-sm' htmlFor="Minimum">Minimum Customers per Hours</label>
+                    <input name='minimum' id='Minimum' type="text" className='flex inline-flex justify-center mx-8' />
+                  </span>
+                  <span className='gap-x-px'>
+                    <label className='text-[0.8rem] flex justify-center text-sm' htmlFor="Maximum"><h5>Maximum Customers per Hours</h5></label>
+                    <input name='maximum' id='Maximum' type="text" className='flex inline-flex justify-center mx-8' />
+                  </span>
+                  <span >
+                    <label name='' className='text-[0.8rem] flex justify-center text-sm' htmlFor="Average">Average Cookies Per Sale</label>
+                    <input name='average' id='Average' type="text" className='flex inline-flex justify-center mx-8' />
+                  </span>
+                  <span>
+                    <button className='h-18 w-40 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 m-2 bg-teal-900 p-3'>Create</button>
+                  </span>
+                </div>
+              </form>
+            </div>
+          </div>
+          <h3 className='flex justify-center text-[0.8rem]'>Report Table Coming Soon...</h3>
+          <div className='flex justify-center'>
+            <p>&#123; "location":"{location}", "minCustomers":"{minCustomers}", "maxCustomers":"{maxCustomers}", "avgCookies":"{avgCoockies}",  &#125;</p>
+          </div>
+        </main>
+        <footer>
+          <div className='flex justify-between bg-teal-600 py-4 items-center'>
+            <p className='text-1xl font-bold mx-1'>&copy; 2021</p>
+          </div>
+        </footer>
+      </body>
+    </>
   )
 }
